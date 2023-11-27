210b7aa30f3f5bb9cde0d973f22eb2ba3253f346
Austin_Curran <a.curran1192@gmail.com>
2023-11-27 10:35:32 -0700

    added card to about page

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,6 +1,8 @@
 @include ('partials._header')
-<div class="text">
-    <h3>About:</h3>
-    <p>This is a clone of my first feedback application. Written with laravel this time around!</p>
+<div class="row">
+    <div class="card -align-center">
+        <h3>About</h3>
+        <p class="-align-center">This is a clone of my first feedback application. Written with laravel this time around!</p>
+    </div>
 </div>
 @include ('partials._footer')
